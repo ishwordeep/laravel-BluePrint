8a8bc18040f7118f3744ab937f0f8c59da20d47d
unknown <ishwor055@gmail.com>
2020-01-01 20:27:48 +0545

    added view for student and StudentController with resources are created

---
+++ resources/views/student-admissionForm.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="/student/create" method="post">
+        <h1>Add Student</h1>
+        <input type="text" name="name" id="name" placeholder="Name"><br/>
+        <input type="text" name="address" id="address" placeholder="Address"><br/>
+        <input type="email" name="email" id="email" placeholder="Email"><br/>
+        <input type="text" name="contact" id="contact" placeholder="Mobile Number"><br/>
+        <input type="text" name="username" id="username" placeholder="Username"><br/>
+        <input type="password" name="password" id="password" placeholder="Password"><br/>
+    </form>
+</body>
+</html>
